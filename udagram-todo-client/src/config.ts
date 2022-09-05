136cd7999bba6cd79e1122de9f5bb879a83c3838
Joel Okoromi <okmarq@gmail.com>
2022-09-05 01:51:16 +0100

    frontend configured

--- udagram-todo-client/src/config.ts
+++ udagram-todo-client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-ssi-vt5b.us.auth0.com',            // Auth0 domain
+  clientId: 'v06UnUGiLHUisW6UZtyvVq1rv7oklbvM',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
