e180acc52288b92763b11d6790b401106a4e01ad
Joel Okoromi <okmarq@gmail.com>
2022-09-05 01:51:26 +0100

    get todo updated

--- udagram-todo-backend/src/lambda/http/getTodos.ts
+++ udagram-todo-backend/src/lambda/http/getTodos.ts
@@ -11,12 +11,50 @@ import { getUserId } from '../utils';
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    await this.dynamoDBClient
+      .query({
+        TableName: process.env.TODOS_TABLE,
+        IndexName: process.env.TODOS_CREATED_AT_INDEX,
+        KeyConditionExpression: 'paritionKey = :paritionKey',
+        ExpressionAttributeValues: {
+          ':paritionKey': partitionKeyValue
+        }
+      })
+      .promise()
 
-    return undefined
+    const todos = {
+      "items": [
+        {
+          "todoId": "123",
+          "createdAt": "2019-07-27T20:01:45.424Z",
+          "name": "Buy milk",
+          "dueDate": "2019-07-29T20:01:45.424Z",
+          "done": false,
+          "attachmentUrl": "http://example.com/image.png"
+        },
+        {
+          "todoId": "456",
+          "createdAt": "2019-07-27T20:01:45.424Z",
+          "name": "Send a letter",
+          "dueDate": "2019-07-29T20:01:45.424Z",
+          "done": true,
+          "attachmentUrl": "http://example.com/image.png"
+        },
+      ]
+    }
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        todos
+      })
+    }
+
+    handler.use(
+      cors({
+        credentials: true
+      })
+    )
