a04b8578ab944b021c36254eade51cb3fe7fe33a
Joel Okoromi <okmarq@gmail.com>
2022-09-06 13:53:01 +0100

    get todo updated

--- udagram-todo-backend/src/lambda/http/getTodos.ts
+++ udagram-todo-backend/src/lambda/http/getTodos.ts
@@ -1,60 +1,39 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    await this.dynamoDBClient
+    const userId = getUserId(event)
+    const result = await docClient
       .query({
-        TableName: process.env.TODOS_TABLE,
+        TableName: todosTable,
         IndexName: process.env.TODOS_CREATED_AT_INDEX,
-        KeyConditionExpression: 'paritionKey = :paritionKey',
+        KeyConditionExpression: 'userId = :userId',
         ExpressionAttributeValues: {
-          ':paritionKey': partitionKeyValue
+          ':userId': userId
         }
       })
       .promise()
-
-    const todos = {
-      "items": [
-        {
-          "todoId": "123",
-          "createdAt": "2019-07-27T20:01:45.424Z",
-          "name": "Buy milk",
-          "dueDate": "2019-07-29T20:01:45.424Z",
-          "done": false,
-          "attachmentUrl": "http://example.com/image.png"
-        },
-        {
-          "todoId": "456",
-          "createdAt": "2019-07-27T20:01:45.424Z",
-          "name": "Send a letter",
-          "dueDate": "2019-07-29T20:01:45.424Z",
-          "done": true,
-          "attachmentUrl": "http://example.com/image.png"
-        },
-      ]
-    }
-
+    const todos = result.Items
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
       body: JSON.stringify({
         todos
       })
     }
-
-    handler.use(
-      cors({
-        credentials: true
-      })
-    )
+  }
+)
+handler.use(
+  cors({
+    credentials: true
+  })
+)
