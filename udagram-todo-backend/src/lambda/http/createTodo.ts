b5c4319414e8256784f9b4ece9f78fd22c557634
Joel Okoromi <okmarq@gmail.com>
2022-09-06 13:52:34 +0100

    create todo updated

--- udagram-todo-backend/src/lambda/http/createTodo.ts
+++ udagram-todo-backend/src/lambda/http/createTodo.ts
@@ -1,42 +1,41 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../businessLogic/todos'
 
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     const timestamp = new Date().toISOString()
+    const userId = getUserId(event)
     // TODO: Implement creating a new TODO item
-
     const todoPayload = {
       "createdAt": timestamp,
       ...newTodo,
+      "userId": userId,
       "done": false,
-      "attachmentUrl": "http://example.com/image.png"
+      "attachmentUrl": ""
     }
-
-    await this.dynamoDBClient
+    await docClient
       .put({
-        TableName: process.env.TODOS_TABLE,
-        Todo: todoPayload
+        TableName: todosTable,
+        Item: todoPayload
       })
       .promise()
-
     return {
       statusCode: 201,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
       body: JSON.stringify({
         todoPayload
       })
     }
+  }
 )
-
 handler.use(
   cors({
     credentials: true
