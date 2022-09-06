891ac78b346304552e3c302f672ee5fa64fb68f4
Joel Okoromi <okmarq@gmail.com>
2022-09-06 13:52:53 +0100

    generate uploads updated

--- udagram-todo-backend/src/lambda/http/generateUploadUrl.ts
+++ udagram-todo-backend/src/lambda/http/generateUploadUrl.ts
@@ -1,5 +1,5 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
@@ -10,8 +10,9 @@ import { getUserId } from '../utils'
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+
 
     return undefined
   }
