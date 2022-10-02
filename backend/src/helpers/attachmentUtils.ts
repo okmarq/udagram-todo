8caa79dc295cb3ba45cf11640f688faabb7625ac
Joel Okoromi <okmarq@gmail.com>
2022-10-02 09:25:07 +0100

    file storage implemented

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -5,7 +5,7 @@ import * as AWSXRay from 'aws-xray-sdk'
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
-// TODO: Implement the fileStogare logic
+// TODO: Implement the fileStorage logic
 const s3 = new XAWS.S3()
 
 const imagesBucketName = process.env.IMAGES_S3_BUCKET
